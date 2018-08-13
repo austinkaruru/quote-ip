05ec0ef3637b5739cd6036e6851176eda9d6eca7
austinkaruru <40546311+austinkaruru@users.noreply.github.com>
2018-08-13 10:19:20 +0300

    Update quote-form.component.ts

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import {Quote} from '../quote';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Quote } from "../quote";
 
 @Component({
-  selector: 'app-quote-form',
-  templateUrl: './quote-form.component.html',
-  styleUrls: ['./quote-form.component.css']
+  selector: "app-quote-form",
+  templateUrl: "./quote-form.component.html",
+  styleUrls: ["./quote-form.component.css"]
 })
 export class QuoteFormComponent implements OnInit {
-  newQuote = new Quote(0, '' , '', new Date());
-  @Output() addQuote = new EventEmitter <Quote>();
+  newQuote = new Quote(0, "", "", "", new Date());
+  public newname: string;
+  public newauthor: string;
+  public newquote: string;
+  public newdate: Date;
+  @Output()
+  addQuote = new EventEmitter<Quote>();
 
   submitQuote() {
-    this.addQuote.emit(this.newQuote);
+    const newQuote = new Quote(
+      0,
+      this.newname,
+      this.newquote,
+      this.newauthor,
+      this.newdate
+    );
+    this.addQuote.emit(newQuote);
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
